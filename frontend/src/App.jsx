0eca4c01656e21978da84f8f9402f57bc26de55f
Aurus-Official <russelquinto.official@gmail.com>
2024-09-17 22:03:25 +0800

    Added initial frontend files.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,7 @@
+import Navbar from "./components/Navbar.jsx";
+
+function App() {
+    return <Navbar />;
+}
+
+export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar() {
+    return (
+        <>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </>
+    );
+}
+
+export default Navbar;
