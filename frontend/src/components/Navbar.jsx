c5f9259a09536b9e9c62f9637954a68558a8104d
Aurus-Official <russelquinto.official@gmail.com>
2024-09-23 17:11:32 +0800

    Added assets and navbar.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,28 @@
+import "./../styles/Navbar.css";
+import Logo from "../assets/logo.svg";
+import Profile from "../assets/pfp.svg";
+
 function Navbar() {
     return (
         <>
-            <ul>
-                <li></li>
-                <li></li>
-                <li></li>
+            <ul className="navbar-container">
+                <li className="title-container">
+                    <img className="navbar-logo" src={Logo} alt="logo" />
+                </li>
+                <li className="searchbar-container">
+                    <input
+                        placeholder="SEARCH"
+                        className="navbar-searchbar"
+                        type="text"
+                    />
+                </li>
+                <li className="user-container">
+                    <img
+                        className="navbar-userimg"
+                        src={Profile}
+                        alt="profile"
+                    />
+                </li>
             </ul>
         </>
     );
